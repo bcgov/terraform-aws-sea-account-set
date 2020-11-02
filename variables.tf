f6084d4f5921c75d730121b4ae12ffdde0d24ad5
Shea Phillips <shea.phillips@gmail.com>
2020-11-02 15:09:10 -0800

    Remove cruft.

--- variables.tf
+++ variables.tf
@@ -10,31 +10,6 @@ variable account_email_prefix {
 	description = "The prefix to use for the email accounts associated with the accounts created by the module. Don't change unless you know what you are doing. In other words, don't change."
 }
 
-
-//project = {
-//		identifier = "2xcrjw"
-//		name = "Virtual Clinic"
-//		accounts = [
-//			{
-//				name = "Tools"
-//				environment = "tools"
-//			},
-//			{
-//				name = "Dev"
-//				environment = "dev"
-//			},
-//			{
-//				name = "Test"
-//				environment = "test"
-//			},
-//			{
-//				name = "Prod"
-//				environment = "prod"
-//			}
-//		]
-//	}
-
-
 variable "project" {
 	description = "List of projects that product teams' workloads run within."
 	type = object({
