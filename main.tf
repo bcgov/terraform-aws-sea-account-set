081cf3c09046275ca533b86da11096e68a74d6ab
Shea Phillips <shea.phillips@gmail.com>
2020-11-10 11:02:12 -0800

    Replaced template string with naked string.

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
 
 resource "aws_organizations_account" "project_accounts" {
 	for_each = { for account in var.project.accounts : account.environment => account }
-	role_name = "${var.org_admin_role_name}"
+	role_name = var.org_admin_role_name
 	name  = "${var.project.identifier}-${each.key}"
 	email = "${var.account_email_prefix}-${var.project.identifier}-${each.key}@${var.account_email_domain}"
 	parent_id = local.ous_by_name[each.key].id
