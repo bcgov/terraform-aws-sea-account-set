83f4d61a5b27dddbf5f56579e2df1b061e652b06
Warren Uniewski <warren@autimo.com>
2022-08-19 14:48:00 -0700

    add close_on_deletion setting

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ resource "aws_organizations_account" "project_accounts" {
     Environment = each.key
   }, local.project_tags)
 
+  close_on_deletion = var.close_on_deletion
+
   // necessary so we can import accounts into state if necessary.  without it, TF will "think" it needs to recreate the resource.  @see associated warning in AWS TF provider docs at https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_account
   lifecycle {
     ignore_changes = [role_name]
--- variables.tf
+++ variables.tf
@@ -1,30 +1,36 @@
 
 
-variable account_email_domain {
-	type = string
-	description = "The domain to use as the suffix for the email accounts associated with the accounts created by the module. Don't change unless you know what you are doing. In other words, don't change."
+variable "account_email_domain" {
+  type        = string
+  description = "The domain to use as the suffix for the email accounts associated with the accounts created by the module. Don't change unless you know what you are doing. In other words, don't change."
 }
 
-variable account_email_prefix {
-	type = string
-	description = "The prefix to use for the email accounts associated with the accounts created by the module. Don't change unless you know what you are doing. In other words, don't change."
+variable "account_email_prefix" {
+  type        = string
+  description = "The prefix to use for the email accounts associated with the accounts created by the module. Don't change unless you know what you are doing. In other words, don't change."
 }
 
 variable "project" {
-	description = "List of projects that product teams' workloads run within."
-	type = object({
-		identifier = string
-		name = string
-		tags = map(string)
-		accounts = list(object({
-			name = string
-			environment = string
-		}))
-	})
+  description = "List of projects that product teams' workloads run within."
+  type = object({
+    identifier = string
+    name       = string
+    tags       = map(string)
+    accounts = list(object({
+      name        = string
+      environment = string
+    }))
+  })
 }
 
 variable "org_admin_role_name" {
-	description = "The role name that will be created/set as the default cross-account admin role for accounts within an organization."
-	type = string
-	default = "OrganizationAccountAccessRole"
+  description = "The role name that will be created/set as the default cross-account admin role for accounts within an organization."
+  type        = string
+  default     = "OrganizationAccountAccessRole"
+}
+
+variable "close_on_deletion" {
+  description = "true means that the account will be closed when it is deleted.  false means that the account be removed from the aws org when it is deleted."
+  type        = boolean
+  default     = false
 }
