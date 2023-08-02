68c139af5f43543fe3fd6049b1a72940a4aaa4f7
Warren Uniewski <warren@autimo.com>
2023-08-02 14:50:36 -0700

    ignore changes to email on the account

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_organizations_account" "project_accounts" {
 
   // necessary so we can import accounts into state if necessary.  without it, TF will "think" it needs to recreate the resource.  @see associated warning in AWS TF provider docs at https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_account
   lifecycle {
-    ignore_changes = [role_name]
+    ignore_changes = [role_name, email]
   }
 }
 
